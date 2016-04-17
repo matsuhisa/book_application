561291ead75ea6d3c50024ba366a47f4d42c0293
matsuhisa <matsuhisa+github@gmail.com>
2016-04-17 19:22:36 +0900

    Add validates for campaign_application

--- app/models/campaign_application.rb
+++ app/models/campaign_application.rb
@@ -1,2 +1,13 @@
 class CampaignApplication < ActiveRecord::Base
+  validate :read_book_1_title_presence, :read_book_1_title
+  validates :read_book_2_date, presence: true, if: -> { read_book_2_title.present? }
+
+  def read_book_1_title_presence
+    unless read_book_1_title.blank?
+      unless read_book_1_date.presence
+        errors.add(:read_book_1_title, :read_book_1_title_presence)
+      end
+    end
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
-  resources :campaign_applications, only: [:index, :create, :new] do
-    collection do
-      post '/confirm', to: 'campaign_applications#confirm', as: :confirm
-      get '/complete', to: 'campaign_applications#complete', as: :complete
-    end
-  end
+  resources :campaign_applications
+  # resources :campaign_applications, only: [:index, :create, :new] do
+  #   collection do
+  #     post '/confirm', to: 'campaign_applications#confirm', as: :confirm
+  #     get '/complete', to: 'campaign_applications#complete', as: :complete
+  #   end
+  # end
 end
